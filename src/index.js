bb402a87042fa9337818984b07686d05ad9b2ce4
mostafakamal22 <36553185+mostafakamal22@users.noreply.github.com>
2022-08-20 21:17:11 +0200

    initial projects with redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './components/App'
+import React from "react";
+import "./index.css";
+import App from "./components/App";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./reducers";
+import middleware from "./middleware";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const store = createStore(reducer, middleware);
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
+);
