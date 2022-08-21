70b6af8b3e7ee8f596f66c776d742aa2b43adf09
mostafakamal22 <36553185+mostafakamal22@users.noreply.github.com>
2022-08-21 21:55:55 +0200

    add new tweet functionality

--- src/components/NewTweet.js
+++ src/components/NewTweet.js
@@ -1,9 +1,28 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Redirect } from "react-router-dom";
+import { addTweet } from "../state/actions/tweets";
+import { TweetContext } from "../state/contexts/tweets/tweetsContext";
+import { AuthedUserContext } from "../state/contexts/authedUser/authedUserContext";
+import { saveTweet } from "../utils/api";
 
-export const NewTweet = () => {
+export const NewTweet = ({ id }) => {
+  const { dispatch } = useContext(TweetContext);
+  const authedUser = useContext(AuthedUserContext);
   const [text, setText] = useState("");
   const [toHome, setToHome] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    saveTweet({
+      text,
+      author: authedUser,
+      replyingTo: id,
+    }).then((tweet) => dispatch(addTweet(tweet)));
+
+    setText("");
+    setToHome(id ? false : true);
+  };
 
   if (toHome === true) {
     return <Redirect to="/" />;
@@ -13,7 +32,7 @@ export const NewTweet = () => {
   return (
     <div>
       <h3 className="center">Compose new Tweet</h3>
-      <form className="new-tweet" onSubmit={(e) => e.preventDefault()}>
+      <form className="new-tweet" onSubmit={(e) => handleSubmit(e)}>
         <textarea
           placeholder="What's happening?"
           value={text}
