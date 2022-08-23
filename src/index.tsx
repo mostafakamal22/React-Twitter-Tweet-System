bb21aaaffd631ab354baef2aec0da8544c5b5ca1
mostafakamal22 <36553185+mostafakamal22@users.noreply.github.com>
2022-08-24 01:19:59 +0200

    fix App component path issue

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "./index.css";
-import App from "./components/App";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import store from "./state/app/store";
 import { Provider } from "react-redux";
+import App from "./components/App";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement!);
